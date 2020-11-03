d4571a97a45694adb97e54e7d521edede4d6adc3
atquick <atquick@outlook.com>
2020-11-03 13:58:47 -0500

    added maps, changed about command, changed formatting on memer cog

---
+++ cogs/game_maps.py
@@ -0,0 +1,36 @@
+import discord
+import requests
+from discord.ext import commands
+from discord.ext.commands import command as Command
+from discord.ext.commands import Context, Cog
+
+# https://i.imgur.com/WUvulfC.png - Mira
+# https://i.imgur.com/iR88yIu.png - Polus
+# https://i.imgur.com/eskTHLJ.png - Skeld
+
+class GameMaps(Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @Command(name='skeld', help='Display a map of The Skeld')
+    @commands.has_permissions(send_messages=True)
+    async def skeld(self, ctx: Context):
+        skeld = 'https://i.imgur.com/lQUSrhv.png'
+        await ctx.send(skeld)
+    
+    @Command(name='mira', help='Display a map of MiraHQ')
+    @commands.has_permissions(send_messages=True)
+    async def mira(self, ctx: Context):
+        mira = 'https://i.imgur.com/wdGIyMJ.png'
+        await ctx.send(mira)
+
+    @Command(name='polus', help='Display a map of Polus')
+    @commands.has_permissions(send_messages=True)
+    async def polus(self, ctx: Context):
+        polus = 'https://i.imgur.com/2E2lEpx.png'
+        await ctx.send(polus)
+
+
+def setup(bot):
+    bot.add_cog(GameMaps(bot))
--- cogs/meme.py
+++ cogs/meme.py
@@ -9,7 +9,7 @@ from discord.ext.commands import command as Command
 from discord.ext.commands import Context, Cog
 
 
-class Memer(commands.Cog):
+class Memer(Cog):
 
     def __init__(self, bot):
         self.bot = bot
